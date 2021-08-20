80f27c745c2b1bc38cb64f08a94c9da1a7ec3810
Sergio Nogueira <sergio.nogueira@25friday.com>
2021-08-20 11:19:15 +0100

    Add AsyncRequest

---
+++ src/Core/Http/AsyncRequest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace QuickBooksOnline\API\Core\Http;
+
+class AsyncRequest
+{
+    /** @var string */
+    private $id;
+
+    /** @var string */
+    private $url;
+
+    /** @var string */
+    private $method;
+
+    /** @var array */
+    private $headers;
+
+    /** @var string */
+    private $body;
+
+    /** @var null|string */
+    private $response;
+
+    /** @var int */
+    private $timeout;
+
+    /** @var bool */
+    private $verifySsl;
+
+    /**
+     * @param string $id
+     * @param string $url
+     * @param string $method
+     * @param array $headers
+     * @param string $body
+     * @param int $timeout
+     * @param bool $verifySsl
+     */
+    public function __construct(
+        $id,
+        $url,
+        $method,
+        array $headers,
+        $body,
+        $timeout,
+        $verifySsl
+    ) {
+        $this->id = $id;
+        $this->url = $url;
+        $this->method = strtoupper($method);
+        $this->headers = $headers;
+        $this->body = $body;
+        $this->timeout = $timeout;
+        $this->verifySsl = $verifySsl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimeout()
+    {
+        return $this->timeout;
+    }
+
+    /**
+     * @return bool
+     */
+    public function verifySsl()
+    {
+        return $this->verifySsl;
+    }
+
+    /**
+     * @param string $response
+     */
+    public function setResponse($response)
+    {
+        $this->response = $response;
+    }
+}
